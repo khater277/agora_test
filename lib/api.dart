bad49a828d6fb1f2d505e90139ace581196d84e7
khater277 <ahmedkhater142000@gmail.com>
2022-07-12 02:12:09 +0200

    RECEIVE CALL

--- lib/api.dart
+++ lib/api.dart
@@ -19,12 +19,14 @@ class DioHelper {
   }
 
 
-  static Future<Response> pushNotification(){
+  static Future<Response> pushNotification({
+  required String token,
+  required String channelName,
+}){
 
     Map<String,dynamic> data = {
-      "to":"dzbZwQuBSMaOTdzC_4qKNo:APA91bGeJ5NPcvi6LXMqtFb9-tUk5f6gHzdOHP6IbL2m4mEtK4Qdem6QXHjKZTFeWV9ejoUE8X2I0C8WhCBJw_GQH-P_cNYzjWma1yBQ0WwNfaGqB4VjW8rI4FG4XiSF4lUt5yi4zyA8",
+      "to":"f6Pr-8neTaiBaNNwIQyzU5:APA91bEwD-BJm_dCDWqUEqx8AsfaFe8kVEHPuoxQJ2_TGATia1Abt2qOLn7BFUCFFerHHU3io8QL8-mJKnd2xXDudIDHQjUlZvS5rTltHGbzQbmEVK0h63ir9ItYpz7lQZ-DDbd86yhD",
       "priority":"high",
-
       "notification":{
         "title":"New Message",
         "body":"user sent you new message",
@@ -32,6 +34,8 @@ class DioHelper {
       },
       "data":{
         "type":"order",
+        "token":token,
+        "channelName":channelName,
         "id":"1",
         "senderID":"123",
         "phoneNumber":"+201000482644",
